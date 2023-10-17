8fd7f446ab95f4d0320fa04bb46685cf3a372082
jjaison <joeljaison13@berkeley.edu>
2023-10-17 13:15:07 -0700

    basic styling for login page

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
+import "./Login.css";
 const Login = () => {
-	return (
-		<p>Login Page</p>
-	)
-}
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleSubmit = (e) => {
+	e.preventDefault();
+  }
+
+  return (
+    <div className="login-wrapper">
+      <div className="login-header" />
+      <div className="login-text">Login</div>
+      <div className="login-input">
+        <form onClick={handleSubmit}>
+          <label className="login-label">Username</label>
+          <input
+            name="username"
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+			className="login-input-form"
+          />
+          <br />
+          <label className="login-label">Name:</label>
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+			className="login-input-form"
+          />
+          <br />
+		  <input 
+          type='submit' 
+          value='Sign In' 
+		  className="signin-button"
+        />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
