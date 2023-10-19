1b605db8c75278f806531c7e8d8ddd0e40406b36
WenWenGu26 <112829010+WenWenGu26@users.noreply.github.com>
2023-10-18 21:06:54 -0700

    first

--- frontend/src/components/Chatbot/Chatbot.js
+++ frontend/src/components/Chatbot/Chatbot.js
@@ -1,7 +1,22 @@
-const Chatbot = () => {
-	return (
-		<p>Chatbot Page</p>
-	)
-}
+import "./Chatbot.css"
+import React from "react";
 
-export default Chatbot
+
+export const ChatBot = () => {
+  return (
+    <div className="chat-bot">
+      <div className="div">
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="text-wrapper">Enter Your Question/upload files...</div>
+        </div>
+        <div className="overlap-group">
+          <div className="text-wrapper-2">ChatBot</div>
+          <p className="p">Welcome! What can I help you study with today?</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBot;
